6da9c848bbbad269e54e9aedbe9cb6e3e66a5e41
Sergiu Uifalean <sergiu.uifalean@studocu.com>
2024-09-19 13:11:05 +0200

    vitest setup

---
+++ src/App.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+describe('App', () => {
+    it('renders the App component', async () => {
+        render(<App />);
+        const title = await screen.findByText('Famly check-in management');
+        expect(title).toBeTruthy();
+    });
+});
